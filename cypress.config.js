20d03573b67d2677aedd691b5f73d8d77165891d
sasha_polianskaia <polianskaia27@gmail.com>
2024-11-29 13:51:13 -0300

    Homework 11.1.1

--- cypress.config.js
+++ cypress.config.js
@@ -4,19 +4,31 @@ const createEsbuildPlugin =
   require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 const addCucumberPreprocessorPlugin =
   require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://staging.lpitko.ru",
     testIsolation: false,
     watchForFileChanges: false,
+    defaultCommandTimeout: 10000,
     specPattern: "**/*.feature",
-    setupNodeEvents(on, config) {
+    setupNodeEvents(cypressOn, config) {
+      const on = require("cypress-on-fix")(cypressOn);
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
       });
       on("file:preprocessor", bundler);
       addCucumberPreprocessorPlugin(on, config);
+      on("task", {
+        reportAllureCypressSpecMessages() {
+          console.log("Reporting Allure Cypress Spec Messages");
+          return null;
+        },
+      });
+
+      allureCypress(on, config);
+
       return config;
     },
   },
